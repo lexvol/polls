8047b01c4b24ba1eb6adc919ba8842e7e5b9bb30
lex <lex.voloskov@gmail.com>
2023-04-05 00:22:49 +0400

    feat: add Question & Choice models

--- balanceWheel/models.py
+++ balanceWheel/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, verbose_name='Вопрос')
+    pub_date = models.DateTimeField(verbose_name='Дата_публикации')
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='id вопроса')
+    choice_text = models.CharField(max_length=200, verbose_name='Вариант ответа')
+    votes = models.IntegerField(default=0, verbose_name='Выбор')
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    class Meta:
+        verbose_name = 'выбор'
+        verbose_name_plural = 'выбор'
