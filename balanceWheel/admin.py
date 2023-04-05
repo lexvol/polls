1cf65a91eee52739c0c0bd60e8da0c65e64fee67
lex <lex.voloskov@gmail.com>
2023-04-05 22:40:32 +0400

    feat: add Choice object in admin interface

--- balanceWheel/admin.py
+++ balanceWheel/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
 
 @admin.register(Question)
-class Question(admin.ModelAdmin):
+class QuestionAdmin(admin.ModelAdmin):
     list_display = ['id', 'question_text', 'pub_date']
     list_filter = ['id', 'pub_date']
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'choice_text', 'votes']
