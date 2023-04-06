cd434e949c06e9b2f66e1f638e8ab62165dc9cfd
lex <lex.voloskov@gmail.com>
2023-04-06 15:59:05 +0400

    feat: added & update Category, Subcategory models

--- balanceWheel/admin.py
+++ balanceWheel/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Choice, Category, Subcategory
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'question_text', 'pub_date']
-    list_filter = ['id', 'pub_date']
-
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Категории', {'fields': ['category', 'subcategory']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'category', 'subcategory')
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ['id', 'question', 'choice_text', 'votes']
+    list_display = ['id', 'question', 'choice_text', 'scores', 'votes']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category_name']
+
+
+@admin.register(Subcategory)
+class Subcategory(admin.ModelAdmin):
+    list_display = ['id', 'subcategory_name']
--- balanceWheel/models.py
+++ balanceWheel/models.py
@@ -1,8 +1,44 @@
 from django.db import models
 
 
+TRUE_FALSE_CHOICES = (
+        (True, 'Да'),
+        (False, 'Нет')
+    )
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30, verbose_name='Категория')
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Subcategory(models.Model):
+    subcategory_name = models.CharField(max_length=30, verbose_name='Подкатегория')
+
+    def __str__(self):
+        return self.subcategory_name
+
+    class Meta:
+        verbose_name = 'подкатегория'
+        verbose_name_plural = 'подкатегории'
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name='Вопрос')
+    category = models.ForeignKey(Category,
+                                 related_name='categoty_name',
+                                 on_delete=models.CASCADE,
+                                 verbose_name='Категория')
+    subcategory = models.ForeignKey(Subcategory,
+                                    related_name='subcategoty_name',
+                                    on_delete=models.CASCADE,
+                                    verbose_name='Подкатегория')
     pub_date = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
 
     def __str__(self):
@@ -14,13 +50,14 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='id вопроса')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
     choice_text = models.CharField(max_length=200, verbose_name='Вариант ответа')
-    votes = models.IntegerField(default=0, verbose_name='Выбор')
+    scores = models.FloatField(default=0.0, verbose_name='Баллы')
+    votes = models.BooleanField(verbose_name='Ответ', choices=TRUE_FALSE_CHOICES)
 
     def __str__(self):
         return self.choice_text
 
     class Meta:
-        verbose_name = 'выбор'
-        verbose_name_plural = 'выбор'
+        verbose_name = 'ответ'
+        verbose_name_plural = 'ответ'
